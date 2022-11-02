9a7326d24d909b2d238b2c6773c7062c535fc85d
David Hermann <david.stefan.hermann@gmail.com>
2022-11-02 02:28:25 +0100

    started experimenting, looking good so far

--- main.py
+++ main.py
@@ -10,6 +10,75 @@ from numpy import any as np_any
 from past.builtins import raw_input
 from screeninfo import get_monitors
 
+
+def get_monitor_sizes() -> list:
+    sizes = []
+    for m in get_monitors():
+        sizes.append([m.width_mm, m.height_mm, m.is_primary])
+    return sizes
+
+
+def monitor_str(size: list) -> str:
+    prim = ""
+    if size[2]:
+        prim = " Primary"
+    return "[w: " + str(size[0]) + " h: " + str(size[1]) + prim + "]"
+
+
+def choose_arrangement(sizes: list) -> list:
+    new_arrangement = []
+    if len(sizes) == 1:
+        return sizes
+    if len(sizes) > 3:
+        print("this program currently only supports up to three monitors.\n")
+    # list monitors
+
+    for j in range(len(sizes) - 1):
+        # list monitors
+        for i, s in enumerate(sizes):
+            print("(" + str(i + 1) + ") " + monitor_str(s))
+        # let user choose left monitor
+        left_most_monitor = input("Choose the left most monitor: ")
+        if not left_most_monitor.isnumeric() or int(left_most_monitor) < 1:
+            print("please enter a valid number.")
+            exit()
+        if int(left_most_monitor) > len(sizes):
+            print("there are not that many monitors.")
+            exit()
+        # input should now be a valid number ( > 0, < len sizes )
+        new_arrangement.append(sizes[int(left_most_monitor) - 1])
+        sizes.remove(sizes[int(left_most_monitor) - 1])
+    # append last element of sizes
+    new_arrangement.extend(sizes)
+    return new_arrangement
+
+
+def max_image_size_mm(sizes: list) -> list:
+    width = 0
+    # init with first monitor height
+    height = sizes[0][1]
+    for s in sizes:
+        width += s[0]
+        if s[1] > height:
+            height = s[1]
+    # border between monitors, each 20mm
+    width += (len(sizes) - 1) * 20
+    return [width, height]
+
+
+if __name__ == "__main__":
+    # get monitors
+    monitor_sizes = get_monitor_sizes()
+    # arrange monitors
+    arranged_sizes = choose_arrangement(monitor_sizes)
+    # get max possible image size / ratio
+    max_size = max_image_size_mm(arranged_sizes)
+
+    print(str(monitor_sizes))
+    print(str(arranged_sizes))
+    print(str(max_size))
+    exit()
+
 x = 10
 
 # Load Config File For Monitor Dimensions
